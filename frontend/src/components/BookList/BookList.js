e1c9aa2283113347e1af800d7ddba121d1859f5a
Bogdan Stashchuk <bstashchuk@gmail.com>
2023-09-19 14:46:39 +0300

    Toggle favorite book

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { deleteBook } from '../../redux/books/actionCreators';
+import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
+import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
 import './BookList.css';
 
 const BookList = () => {
@@ -8,6 +9,10 @@ const BookList = () => {
 
     const handleDeleteBook = (id) => {
         dispatch(deleteBook(id));
+    };
+
+    const handleToggleFavorite = (id) => {
+        dispatch(toggleFavorite(id));
     };
 
     return (
@@ -24,6 +29,17 @@ const BookList = () => {
                                 <strong>{book.author}</strong>
                             </div>
                             <div className="book-actions">
+                                <span
+                                    onClick={() =>
+                                        handleToggleFavorite(book.id)
+                                    }
+                                >
+                                    {book.isFavorite ? (
+                                        <BsBookmarkStarFill className="star-icon" />
+                                    ) : (
+                                        <BsBookmarkStar className="star-icon" />
+                                    )}
+                                </span>
                                 <button
                                     onClick={() => handleDeleteBook(book.id)}
                                 >
--- frontend/src/redux/books/reducer.js
+++ frontend/src/redux/books/reducer.js
@@ -8,6 +8,12 @@ const booksReducer = (state = initialState, action) => {
             return [...state, action.payload];
         case a.DELETE_BOOK:
             return state.filter((book) => book.id !== action.payload);
+        case a.TOGGLE_FAVORITE:
+            return state.map((book) =>
+                book.id === action.payload
+                    ? { ...book, isFavorite: !book.isFavorite }
+                    : book
+            );
         default:
             return state;
     }
