d790e04f41c0915d2146cb3d64391f60f0b6ff1d
Bogdan Stashchuk <bstashchuk@gmail.com>
2023-09-21 15:36:16 +0300

    Filter by author

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,12 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
-import { selectTitleFilter } from '../../redux/slices/filterSlice';
+import {
+    selectTitleFilter,
+    selectAuthorFilter,
+} from '../../redux/slices/filterSlice';
 import './BookList.css';
 
 const BookList = () => {
     const books = useSelector((state) => state.books);
     const titleFilter = useSelector(selectTitleFilter);
+    const authorFilter = useSelector(selectAuthorFilter);
     const dispatch = useDispatch();
 
     const handleDeleteBook = (id) => {
@@ -21,7 +25,10 @@ const BookList = () => {
         const matchesTitle = book.title
             .toLowerCase()
             .includes(titleFilter.toLowerCase());
-        return matchesTitle;
+        const matchesAuthor = book.author
+            .toLowerCase()
+            .includes(authorFilter.toLowerCase());
+        return matchesTitle && matchesAuthor;
     });
 
     return (
