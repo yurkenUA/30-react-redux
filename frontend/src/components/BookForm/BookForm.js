b10dc06103b0ecc2991dd148875245d51de66026
Bogdan Stashchuk <bstashchuk@gmail.com>
2023-09-22 17:50:27 +0300

    Dispatch error in the async thunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -30,7 +30,7 @@ const BookForm = () => {
     };
 
     const handleAddRandomBookViaAPI = () => {
-        dispatch(fetchBook());
+        dispatch(fetchBook('http://localhost:4000/random-book'));
     };
 
     return (
