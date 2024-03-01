b22d8bc946aa6280e2839e1377909c8e57a61e45
Yurii Zatoka <yuriy.zatoka@gmail.com>
2024-02-29 16:39:17 -0800

    Initial React App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,3 @@
-import BookList from './components/BookList/BookList';
-import BookForm from './components/BookForm/BookForm';
-import Filter from './components/Filter/Filter';
-import Error from './components/Error/Error';
 import './App.css';
 
 function App() {
@@ -10,16 +6,7 @@ function App() {
             <header className="app-header">
                 <h1>Book Library App</h1>
             </header>
-            <main className="app-main">
-                <div className="app-left-column">
-                    <BookForm />
-                </div>
-                <div className="app-right-column">
-                    <Filter />
-                    <BookList />
-                </div>
-            </main>
-            <Error />
+
         </div>
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom/client';
-import store from './redux/store';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
